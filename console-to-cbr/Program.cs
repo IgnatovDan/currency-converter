8b0278f1dae7be516bb44003317f76cb7fcea73a
Ignatov Dan <ignatovdan@gmail.com>
2022-07-25 16:13:01 +0300

    feat(console): add no command line arguments message

--- console-to-cbr/Program.cs
+++ console-to-cbr/Program.cs
@@ -7,8 +7,12 @@ try {
   Console.OutputEncoding = System.Text.Encoding.UTF8;
   var exchangeRates = await ConsoleToCBR.CRBUtils.GetExchangeRates();
 
-  Console.WriteLine($"Total rates - {exchangeRates?.Items.Count}, rates date - {exchangeRates?.Date}");
-  Console.WriteLine($"rates[0]: ID - {exchangeRates?.Items[0].ID}, Name - {exchangeRates?.Items[0].Name}");
+  if (args.Length == 0) {
+    Console.WriteLine("Arguments: source_currency_code amount target_currency_code");
+    Console.WriteLine("For example: RUB 1000 USD");
+  }
+  Console.WriteLine($"Total rates available: {exchangeRates?.Items.Count}, rates date: {exchangeRates?.Date}");
+  Console.WriteLine($"rates[0]: Code - {exchangeRates?.Items[0].CharCode}, Name - {exchangeRates?.Items[0].Name}");
 }
 catch (Exception e) {
   Console.WriteLine($"Error occurs: {e.Message}");
