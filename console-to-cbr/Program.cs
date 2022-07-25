53e08792eb46c4a0e14426afd9fe5ebe0c2a2c3b
Ignatov Dan <ignatovdan@gmail.com>
2022-07-25 15:50:08 +0300

    fix(console): '???' are shown for RU characters

--- console-to-cbr/Program.cs
+++ console-to-cbr/Program.cs
@@ -2,6 +2,7 @@
 // dotnet run
 
 try {
+  Console.OutputEncoding = System.Text.Encoding.UTF8;
   var exchangeRates = await ConsoleToCBR.CRBUtils.GetExchangeRates();
 
   Console.WriteLine($"Total rates - {exchangeRates?.Items.Count}, rates date - {exchangeRates?.Date}");
