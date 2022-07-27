22a62e01a891895bbebcad48790822bcc4a46c24
Ignatov Dan <ignatovdan@gmail.com>
2022-07-27 14:00:07 +0300

    refactor(react-cbr): Move App to a separate folder

--- react-to-cbr/src/index.js
+++ react-to-cbr/src/index.js
@@ -8,7 +8,7 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 
 import './index.css';
-import App from './components/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
