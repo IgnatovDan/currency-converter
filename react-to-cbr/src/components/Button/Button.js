2dd08a34cd89bd93a07d9e35d1cd7aa590dc64f4
Ignatov Dan <ignatovdan@gmail.com>
2022-07-28 09:31:40 +0300

    refactor(react-cbr): embed svg image into markup

--- react-to-cbr/src/components/Button/Button.js
+++ react-to-cbr/src/components/Button/Button.js
@@ -1,12 +1,13 @@
 import './button.css';
 import './__image/button__image.css';
 import './__text/button__text.css';
+import { ReactComponent as UpDownArrows } from '../../images/up-down-arrows.svg'; /* from https://uxwing.com/up-down-arrows-icon/ */
 
 function Button({ classes, onClick }) {
   // TODO: pass image and caption as markup
   return (
     <button className={ `${classes} button` } type="button" onClick={ onClick }>
-      <div className="button__image"></div>
+      <UpDownArrows className="button__image"/>
       <div className="button__text">Toggle currencies</div>
     </button>
   );
