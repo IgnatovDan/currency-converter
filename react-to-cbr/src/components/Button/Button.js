166bbe0d11b6d54b09f279d5e7cd431fbf2e7cbd
Ignatov Dan <ignatovdan@gmail.com>
2022-07-28 09:32:50 +0300

    docs(react-cbr): add link to 'images in react' topic

--- react-to-cbr/src/components/Button/Button.js
+++ react-to-cbr/src/components/Button/Button.js
@@ -1,6 +1,8 @@
 import './button.css';
 import './__image/button__image.css';
 import './__text/button__text.css';
+
+// https://create-react-app.dev/docs/adding-images-fonts-and-files
 import { ReactComponent as UpDownArrows } from '../../images/up-down-arrows.svg'; /* from https://uxwing.com/up-down-arrows-icon/ */
 
 function Button({ classes, onClick }) {
