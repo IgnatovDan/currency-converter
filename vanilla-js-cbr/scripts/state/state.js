cfb7768a72173322e82cbb98c448af764906136c
Ignatov Dan <ignatovdan@gmail.com>
2022-07-28 10:43:16 +0300

    fix(vanilla-cbr): fix width for long captions

--- vanilla-js-cbr/scripts/state/state.js
+++ vanilla-js-cbr/scripts/state/state.js
@@ -2,17 +2,20 @@ class Currency {
   #name;
   #charCode;
   #value;
+
   constructor(name, charCode, value) {
     this.#name = name; // 'String', human readable name
     this.#charCode = charCode; // 'String', code
     this.#value = value; // 'Number' TODO: force convert to number
   }
+
   get Name() { return this.#name; }
   get CharCode() { return this.#charCode; }
   get Value() { return this.#value; }
-  static RUB() {
-    return new Currency("Российский рубль", "RUB", 1);
-  }
+
+  static RUB() { return new Currency("Российский рубль", "RUB", 1); }
+  static USD() { return new Currency("Доллар США", "USD", 60.2198); }
+  static GBP() { return new Currency("Фунт стерлингов Соединенного королевства", "GBP", 72.5287); }
 }
 
 class ExchangeRates {
@@ -66,6 +69,17 @@ class State {
 
   #targetRate = 0;
   #targetRateChanged = new EventTarget();
+
+  constructor() {
+    this.#amount = 42;
+    this.#availableCurrencies = [Currency.RUB(), Currency.USD(), Currency.GBP()];
+    this.#sourceCurrencyCharCode = this.#availableCurrencies[0].CharCode;
+    this.#sourceCurrencyValue = this.#availableCurrencies[0].Value;
+    this.#targetCurrencyCharCode = this.#availableCurrencies[1].CharCode;
+    this.#targetCurrencyValue = this.#availableCurrencies[1].Value;
+    this.refreshTargetAmount(this);
+    this.refreshTargetRate(this);
+  }
 
   get availableCurrencies() { return this.#availableCurrencies; }
   get availableCurrenciesChanged() { return this.#availableCurrenciesChanged; }
