036f883bde0781199037f278064a6242c77cddd1
Ignatov Dan <ignatovdan@gmail.com>
2022-07-28 10:46:36 +0300

    chore(react-cbr): add GBR to initial items

--- react-to-cbr/src/components/Converter/converter-model.js
+++ react-to-cbr/src/components/Converter/converter-model.js
@@ -4,20 +4,20 @@ export class Currency {
   #name;
   #charCode;
   #value;
+
   constructor(name, charCode, value) {
     this.#name = name; // 'String', human readable name
     this.#charCode = charCode; // 'String', code
     this.#value = value; // 'Number' TODO: force convert to number
   }
+
   get Name() { return this.#name; }
   get CharCode() { return this.#charCode; }
   get Value() { return this.#value; }
-  static RUB() {
-    return new Currency("Российский рубль", "RUB", 1);
-  }
-  static USD() {
-    return new Currency("Доллар США", "USD", 60.2198);
-  }
+
+  static RUB() { return new Currency("Российский рубль", "RUB", 1); }
+  static USD() { return new Currency("Доллар США", "USD", 60.2198); }
+  static GBP() { return new Currency("Фунт стерлингов Соединенного королевства", "GBP", 72.5287); }
 }
 
 export class ExchangeRates {
@@ -50,8 +50,7 @@ export class ConverterModel {
 
   constructor() {
     this.amount = 42;
-    this.targetAmount = this.amount;
-    this.availableCurrencies = [Currency.RUB(), Currency.USD()];
+    this.availableCurrencies = [Currency.RUB(), Currency.USD(), Currency.GBP()];
     this.sourceCurrencyCharCode = this.availableCurrencies[0].CharCode;
     this.sourceCurrencyValue = this.availableCurrencies[0].Value;
     this.targetCurrencyCharCode = this.availableCurrencies[1].CharCode;
@@ -106,7 +105,7 @@ export class ConverterModel {
     } else {
       return ConverterModel.Reduce(
         model,
-        { availableCurrencies: currencies /* TODO: set Source/Target to currencies[0] */},
+        { availableCurrencies: currencies /* TODO: set Source/Target to currencies[0] */ },
         (model) => {
           ConverterModel.#refreshTargetAmount(model);
           ConverterModel.#refreshTargetRate(model);
