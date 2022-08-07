e5497b54aac8277bc78d961f84aa92daacda719b
Ignatov Dan <ignatovdan@gmail.com>
2022-08-07 21:38:34 +0300

    chore(console): add ExchangeSourcesManager.GetExchangeRates tests

--- console_tests/currency-converter-lib/exchange-sources/exchange-sources-manager.cs
+++ console_tests/currency-converter-lib/exchange-sources/exchange-sources-manager.cs
@@ -6,19 +6,15 @@ namespace CurrencyConverter.ExchangeRateSources {
   public class ExchangeSourcesManager {
     Dictionary<string, IExchangeSource> sources { get; } = new Dictionary<string, IExchangeSource>();
 
-    public void RegisterSource(string sourceName!!, IExchangeSource exchangeSource) {
-      if (string.IsNullOrEmpty(sourceName)) {
-        throw new ArgumentException($"'{nameof(sourceName)}' cannot be null or empty.", nameof(sourceName));
-      }
-
-      if (exchangeSource is null) {
-        throw new ArgumentNullException(nameof(exchangeSource));
+    public void RegisterSource(string sourceName!!, IExchangeSource exchangeSource!!) {
+      if (sourceName == String.Empty) {
+        throw new ArgumentNullException($"'{nameof(sourceName)}' cannot be null or empty.", nameof(sourceName));
       }
 
       this.sources.Add(sourceName, exchangeSource);
     }
 
-    public async Task<ExchangeRates> GetExchangeRates(string sourceName) {
+    public Task<ExchangeRates> GetExchangeRates(string sourceName!!) {
       IExchangeSource? exchangeRatesSource;
       if (!sources.TryGetValue(sourceName, out exchangeRatesSource) || (exchangeRatesSource == null)) {
         throw new Exception($"Cannot find '{sourceName}' rates source. Available sources: {String.Join(", ", sources.Keys)}.");
@@ -26,7 +22,7 @@ namespace CurrencyConverter.ExchangeRateSources {
       if (exchangeRatesSource == null) {
         throw new Exception($"A 'null' value is received by the '{sourceName}' rates source name.");
       }
-      return await exchangeRatesSource.getRates();
+      return exchangeRatesSource.getRates();
     }
   }
 }
--- console_tests/currency-converter-tests/exchange-sources-manager-tests.cs
+++ console_tests/currency-converter-tests/exchange-sources-manager-tests.cs
@@ -1,12 +1,8 @@
 using CurrencyConverter.ExchangeRateSources;
 
+using Moq;
+
 namespace CurrencyConverterTests {
-  class ExchangeSourceMock : IExchangeSource {
-    public Func<Task<ExchangeRates>>? getRates = null;
-    Task<ExchangeRates> IExchangeSource.getRates() {
-      return (getRates == null) ? new Task<ExchangeRates>(() => new ExchangeRates(null)) : getRates();
-    }
-  }
 
   public class ExchangeSourcesManagerTests {
     [Theory, MemberData(nameof(RegisterSource_NullSourceName_Data))]
@@ -16,17 +12,17 @@ namespace CurrencyConverterTests {
     }
     public static IEnumerable<object?[]> RegisterSource_NullSourceName_Data => new List<object?[]> {
       new object?[] { null, null },
-      new object?[] { null, new ExchangeSourceMock() },
+      new object?[] { null, new Mock<IExchangeSource>().Object },
     };
 
     [Theory, MemberData(nameof(RegisterSource_EmptyStringSourceName_Data))]
     public void RegisterSource_EmptyStringSourceName(string sourceName, IExchangeSource exchangeSource) {
       var manager = new ExchangeSourcesManager();
-      Assert.Throws<System.ArgumentException>(() => manager.RegisterSource(sourceName, exchangeSource));
+      Assert.Throws<System.ArgumentNullException>(() => manager.RegisterSource(sourceName, exchangeSource));
     }
     public static IEnumerable<object?[]> RegisterSource_EmptyStringSourceName_Data => new List<object?[]> {
       new object?[] { "", null },
-      new object?[] { "", new ExchangeSourceMock() },
+      new object?[] { "", new Mock<IExchangeSource>().Object },
     };
 
     [Theory, MemberData(nameof(RegisterSource_NullSource_Data))]
@@ -37,5 +33,44 @@ namespace CurrencyConverterTests {
     public static IEnumerable<object?[]> RegisterSource_NullSource_Data => new List<object?[]> {
       new object?[] { null }
     };
+
+    [Theory, MemberData(nameof(GetRates_NullSourceName_Data))]
+    public async void GetRates_NullSourceName(string sourceName) {
+      var manager = new ExchangeSourcesManager();
+      await Assert.ThrowsAsync<System.ArgumentNullException>(() => manager.GetExchangeRates(sourceName));
+    }
+    public static IEnumerable<object?[]> GetRates_NullSourceName_Data => new List<object?[]> {
+      new object?[] { null }
+    };
+
+    [Fact]
+    async public void GetRates_SourceIsNotFound() {
+      var manager = new ExchangeSourcesManager();
+      manager.RegisterSource("1", new Mock<IExchangeSource>().Object);
+      manager.RegisterSource("2", new Mock<IExchangeSource>().Object);
+      var ex = await Assert.ThrowsAsync<System.Exception>(() => manager.GetExchangeRates("3"));
+      Assert.Contains("3", ex.Message);
+    }
+
+    [Fact]
+    async public Task GetRates_BySourceName() {
+      var source1 = new Mock<IExchangeSource>();
+      var rates1 = new ExchangeRates(null);
+      source1.Setup(source => source.getRates()).Returns(Task.FromResult<ExchangeRates>(rates1));
+
+      var source2 = new Mock<IExchangeSource>();
+      var rates2 = new ExchangeRates(null);
+      source2.Setup(source => source.getRates()).Returns(Task.FromResult<ExchangeRates>(rates2));
+
+      var manager = new ExchangeSourcesManager();
+      manager.RegisterSource("1", source1.Object);
+      manager.RegisterSource("2", source2.Object);
+
+      ExchangeRates resultSource1 = await manager.GetExchangeRates("1");
+      Assert.Equal(rates1, resultSource1);
+
+      var resultSource2 = await manager.GetExchangeRates("2");
+      Assert.Equal(rates2, resultSource2);
+    }
   }
 }
