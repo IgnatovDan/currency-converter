6e10ad0bec5f28e03711400122c211604a34e91d
Ignatov Dan <ignatovdan@gmail.com>
2022-08-11 16:39:52 +0300

    Check failure

---
+++ react/src/components/app/_App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './app';
+
+test('renders header', () => {
+  //render(<App />);
+  // const linkElement = screen.getByText(/Currency Converter/i);
+  // expect(linkElement).toBeInTheDocument();
+  expect(true).toBe(false);
+});
