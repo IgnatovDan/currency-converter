48aacbc4aef39049584ad0c808fa4603d5cbb731
Ignatov Dan <ignatovdan@gmail.com>
2022-08-12 01:55:36 +0300

    Store links to jest/react resources

--- react/src/components/app/app.test.js
+++ react/src/components/app/app.test.js
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './app';
 
 test('renders header', () => {
+
+  // Testing In React - https://javascript.plainenglish.io/testing-in-react-part-1-types-tools-244107abf0c6
+  // Jest expect dosc - https://jestjs.io/docs/expect
+  // React Test Library (+ Jest) - https://lburdock.github.io/docs/js-test/
+
   //render(<App />);
   // const linkElement = screen.getByText(/Currency Converter/i);
   // expect(linkElement).toBeInTheDocument();
