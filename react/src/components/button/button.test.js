a0b40dfc0616e03318b94f1045979a9b79f07481
Ignatov Dan <ignatovdan@gmail.com>
2022-08-11 16:45:36 +0300

    rename test

--- react/src/components/button/button.test.js
+++ react/src/components/button/button.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Button from './button';
 
-test('renders header', () => {
+test('render svg tag', () => {
   const { container } = render(<Button svgImage="svg" />);
   const svgElements = container.querySelectorAll('svg');
   expect(svgElements).toHaveLength(1);
