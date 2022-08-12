a18862d19e63408ccb967bf93c6095c10170ffc4
Ignatov Dan <ignatovdan@gmail.com>
2022-08-12 11:10:40 +0300

    Handle warnings

--- react/src/components/editor/editor.test.js
+++ react/src/components/editor/editor.test.js
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Editor from './editor';
 
