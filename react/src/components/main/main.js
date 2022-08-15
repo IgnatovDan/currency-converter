c3f4a5f7019af538693510e3ed308dc3ce868a17
Ignatov Dan <ignatovdan@gmail.com>
2022-08-15 17:12:34 +0300

    Typo

--- react/src/components/main/main.js
+++ react/src/components/main/main.js
@@ -1,4 +1,4 @@
-import ConverterWithRateSources from "../converter-with-sources/converter-with-sources";
+import ConverterWithRateSources from "../converter-with-rate-sources/converter-with-rate-sources";
 
 import styles from './main.module.css';
 import styles__converter from './__converter/main__converter.module.css';
