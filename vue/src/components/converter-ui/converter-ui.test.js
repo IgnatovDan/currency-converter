039f2b21002f0f2e1d0d15d733c99449680c0d40
Ignatov Dan <ignatovdan@gmail.com>
2022-08-18 12:12:06 +0300

    Start 'converter-ui'

---
+++ vue/src/components/converter-ui/converter-ui.test.js
@@ -0,0 +1,202 @@
+import { render, screen, fireEvent } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+import ConverterUI from './converter-ui.vue';
+
+describe('Source amount', () => {
+  const labelMatch = /source amount/i;
+  test('render', () => {
+    render(<ConverterUI />);
+    expect(screen.getByLabelText(labelMatch)).toBeInTheDocument();
+  });
+
+  test('render value', () => {
+    render(<ConverterUI amount={ 42 } />);
+    expect(screen.getByLabelText(labelMatch)).toHaveValue(42);
+  });
+
+  test('amountChanged callback is called when amount was changed', async () => {
+    const user = userEvent.setup()
+    const { emitted } = render(<ConverterUI amount={ 0 } />);
+
+    await user.type(screen.getByLabelText(labelMatch), '5');
+
+    expect(screen.getByLabelText(labelMatch)).toHaveValue(5);
+    expect(emitted('update:amount')[0][0]).toBe(5);
+  });
+});
+
+// describe('From', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByLabelText(/from/i)).toBeInTheDocument();
+//   });
+
+//   test('render value', () => {
+//     const selectCurrencyListItems = [{ value: 'code1', text: 'Name1' }, { value: 'code2', text: 'Name2' }];
+//     render(<ConverterUI selectCurrencyListItems={ selectCurrencyListItems } sourceCurrencyCharCode={ 'code2' } />);
+
+//     expect(screen.getByLabelText(/from/i)).toHaveValue('code2');
+//   });
+
+//   test('sourceCurrencyCharCodeChanged callback is called when source currency was changed', async () => {
+//     const selectCurrencyListItems = [
+//       { value: 'code1', text: 'Name1' },
+//       { value: 'code2', text: 'Name2' },
+//       { value: 'code3', text: 'Name3' }
+//     ];
+//     const { emitted } = render(<ConverterUI selectCurrencyListItems={ selectCurrencyListItems } sourceCurrencyCharCode={ 'code2' } />);
+
+//     await userEvent.selectOptions( screen.getByLabelText(/from/i), screen.getByRole('option', { name: 'code3' }));
+
+//     expect(emitted('update:sourceCurrencyCharCode').length).toBe(1);
+//     expect(emitted('update:sourceCurrencyCharCode')[0][0]).toBe('code3');
+//   });
+// });
+
+// describe('Toggle currencies', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByRole('button', { name: /Toggle currencies/i })).toBeInTheDocument();
+//   });
+
+//   test('click calls sourceCurrencyCharCodeChanged and targetCurrencyCharCodeChanged', async () => {
+//     const user = userEvent.setup()
+//     const selectCurrencyListItems = [{ value: 'code1', text: 'Name1' }, { value: 'code2', text: 'Name2' }];
+
+//     const { emitted } = render(<ConverterUI
+//       selectCurrencyListItems={ selectCurrencyListItems }
+//       sourceCurrencyCharCode={ 'code1' }
+//       targetCurrencyCharCode={ 'code2' }
+//     />);
+
+//     await user.click(screen.getByRole('button', { name: /Toggle currencies/i }));
+
+//     expect(emitted('update:sourceCurrencyCharCode').length).toBe(1);
+//     expect(emitted('update:sourceCurrencyCharCode')[0][0]).toBe('code2');
+
+//     expect(emitted('update:targetCurrencyCharCode').length).toBe(1);
+//     expect(emitted('update:targetCurrencyCharCode')[0][0]).toBe('code1');
+//   });
+// });
+
+// describe('Into', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByLabelText(/into/i)).toBeInTheDocument();
+//   });
+
+//   test('render value', () => {
+//     const selectCurrencyListItems = [{ value: 'code1', Text: 'Name1' }, { value: 'code2', Text: 'Name2' }];
+//     render(<ConverterUI selectCurrencyListItems={ selectCurrencyListItems } targetCurrencyCharCode={ 'code2' } />);
+//     expect(screen.getByLabelText(/into/i)).toHaveValue('code2');
+//   });
+
+//   test('update:targetCurrencyCharCode callback is called when target currency was changed', async () => {
+//     const selectCurrencyListItems = [
+//       { value: 'code1', Text: 'Name1' },
+//       { value: 'code2', Text: 'Name2' },
+//       { value: 'code3', Text: 'Name3' }
+//     ];
+//     const user = userEvent.setup();
+   
+//     const { emitted } = render(<ConverterUI
+//       selectCurrencyListItems={ selectCurrencyListItems }
+//       targetCurrencyCharCode={ 'code2' } />);
+    
+//     await user.selectOptions(screen.getByLabelText(/into/i), 'code3');
+//     //await user.selectOptions( screen.getByLabelText(/into/i), screen.getByRole('option', { name: 'code3' }));
+//     //await userEvent.selectOptions( screen.getByLabelText(/from/i), screen.getByRole('option', { name: 'code3' }));
+
+//     expect(emitted('update:targetCurrencyCharCode').length).toBe(1);
+//     expect(emitted('update:targetCurrencyCharCode')[0][0]).toBe('code3');
+//   });
+// });
+
+// describe('Target amount', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByLabelText(/target amount/i)).toBeInTheDocument();
+//   });
+
+//   test('render value', () => {
+//     render(<ConverterUI targetAmount={ 42 } />);
+//     expect(screen.getByLabelText(/target amount/i).textContent).toBe('42');
+//   });
+// });
+
+// describe('Exchange rates source', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByLabelText(/Exchange rates source/i)).toBeInTheDocument();
+//   });
+
+//   test('render value', () => {
+//     const selectRatesSourceListItems = [
+//       { value: 1, text: 'item1' },
+//       { value: 2, text: 'item2' },
+//       { value: 3, text: 'item3' }
+//     ];
+//     render(<ConverterUI selectRatesSourceListItems={ selectRatesSourceListItems } exchangeRatesSourceKey={ 2 } />);
+//     expect(screen.getByLabelText(/Exchange rates source/i)).toHaveValue('2');
+//   });
+
+//   test('update:exchangeRatesSourceKey callback is called when target rates source was changed', async () => {
+//     const selectRatesSourceListItems = [
+//       { value: 1, text: 'item1' },
+//       { value: 2, text: 'item2' },
+//       { value: 3, text: 'item3' }
+//     ];
+//     const user = userEvent.setup()
+   
+//     const { emitted } = render(<ConverterUI
+//       selectRatesSourceListItems={ selectRatesSourceListItems }
+//       exchangeRatesSourceKey={ 2 } />);
+    
+//     await user.selectOptions(screen.getByLabelText(/Exchange rates source/i), '3');
+
+//     expect(emitted('update:exchangeRatesSourceKey').length).toBe(1);
+//     expect(emitted('update:exchangeRatesSourceKey')[0][0]).toBe('3');
+//   });
+// });
+
+// describe('Currency rate expression', () => {
+//   test('render', () => {
+//     render(<ConverterUI />);
+//     expect(screen.getByLabelText(/Source rate/i)).toBeInTheDocument();
+//     expect(screen.getByLabelText(/Source char code/i)).toBeInTheDocument();
+//     expect(screen.getByLabelText(/Target rate/i)).toBeInTheDocument();
+//     expect(screen.getByLabelText(/Target char code/i)).toBeInTheDocument();
+//   });
+
+//   test('render values', () => {
+//     render(<ConverterUI sourceCurrencyCharCode={ "USD" } targetRate={ 11 } targetCurrencyCharCode={ "RUB" } />);
+//     expect(screen.getByLabelText(/Source rate/i).textContent).toBe('1');
+//     expect(screen.getByLabelText(/Source char code/i).textContent).toBe('USD');
+//     expect(screen.getByLabelText(/Target rate/i).textContent).toBe('11');
+//     expect(screen.getByLabelText(/Target char code/i).textContent).toBe('RUB');
+//   });
+// });
+
+// describe('Warning message', () => {
+//   test('render if not empty', () => {
+//     render(<ConverterUI warningMessage={ 'msg1' } />);
+//     expect(screen.getByLabelText(/Warning message/i).textContent).toBe('msg1');
+//   });
+
+//   test('not render if empty', () => {
+//     render(<ConverterUI />);
+//     expect(screen.queryByLabelText(/Warning message/i)).toBeNull();
+//   });
+// });
+
+// describe('Loading panel', () => {
+//   test('render if loading:true', () => {
+//     render(<ConverterUI isLoading={ true } />);
+//     expect(screen.getByTestId(/loading-panel/i)).toBeInTheDocument();
+//   });
+
+//   test('not render if loading:false', () => {
+//     render(<ConverterUI isLoading={ false } />);
+//     expect(screen.queryByTestId(/loading-panel/i)).toBeNull();
+//   });
+// });
---
+++ vue/src/components/converter-ui/converter-ui.vue
@@ -0,0 +1,201 @@
+<template>
+  <div v-bind="$attrs" class="converter">
+    <form @submit.prevent>
+      <fieldset class="converter__values">
+        <UiEditor aria-label="Source amount" editorType="spinbutton" v-model="amountValue" required :step="0.01" />
+        <LabeledEditor caption="From">
+          <UiEditor
+            editorType="combobox"
+            v-model="sourceCurrencyCharCode"
+            required
+            :listItems="selectCurrencyListItems"
+          />
+        </LabeledEditor>
+        <UiButton
+          class="converter__currency-toggler"
+          :image="toggleIconSrc"
+          text="Toggle currencies"
+          @click="() => handleTogglerClick()"
+        />
+        <LabeledEditor caption="To">
+          <UiEditor
+            editorType="combobox"
+            v-model="targetCurrencyCharCode"
+            required
+            :listItems="selectCurrencyListItems"
+          />
+        </LabeledEditor>
+        <LabeledEditor caption="Exchange rates source">
+          <UiEditor
+            editorType="combobox"
+            v-model="exchangeRatesSourceKey"
+            required
+            :listItems="selectRatesSourceListItems"
+          />
+        </LabeledEditor>
+      </fieldset>
+    </form>
+    <p class="converter__target-amount">{{ targetAmount }}</p>
+    <CurrencyRateExpression
+      :sourceCurrencyCharCode="sourceCurrencyCharCode"
+      :targetRate="targetRate"
+      :targetCurrencyCharCode="targetCurrencyCharCode"
+    />
+    <p v-if="warningMessage" class="converter__warning-message">{{ warningMessage }}</p>
+    <LoadingPanel v-if="isLoading" />
+  </div>
+</template>
+
+<script>
+import UiEditor from '../ui-editor/ui-editor.vue';
+import LabeledEditor from '../labeled-editor/labeled-editor.vue';
+import UiButton from '../ui-button/ui-button.vue';
+import LoadingPanel from '../loading-panel/loading-panel.vue';
+import CurrencyRateExpression from '../currency-rate-expression/currency-rate-expression.vue';
+
+import rateSourcesManager from '../../api/exchange-sources/exchange-sources-manager.js'; // TODO: pass as props/context
+import { Currency } from '../../api/exchange-sources/exchange-rates-data-objects.js';
+
+function convertCurrencyArrayToListItems(currencyArray) {
+  return currencyArray?.map((item) => ({ value: item.CharCode, text: item.Name }));
+}
+
+function HandleValueNumberToZero(value) {
+  if (Number.isNaN(value) || value === Infinity || value === undefined || value === null) {
+    return 0;
+  }
+  return value;
+}
+
+const toggleIconSrc = require('@/assets/up-down-arrows.svg');
+
+export default {
+  components: { UiEditor, LabeledEditor, UiButton, LoadingPanel, CurrencyRateExpression },
+
+  props: {
+    selectCurrencyListItems: {
+      type: Array,
+      default: [],
+      validator: (items) =>
+        items.every((item) => typeof item === 'object' && item !== null && 'value' in item && 'text' in item),
+    },
+    amount: { type: Number, default: 0 },
+    sourceCurrencyCharCode: { type: String },
+    targetCurrencyCharCode: { type: String },
+    targetAmount: { type: Number, default: 0 },
+    targetRate: { type: Number, default: 0 },
+    warningMessage: { type: String },
+    isLoading: { type: Boolean },
+    exchangeRatesSourceKey: { type: String },
+    selectRatesSourceListItems: {
+      type: Array,
+      default: [],
+      validator: (items) =>
+        items.every((item) => typeof item === 'object' && item !== null && 'value' in item && 'text' in item),
+    },
+  },
+
+  emits: [
+    'update:amount',
+    'update:sourceCurrencyCharCode',
+    'update:targetCurrencyCharCode',
+    'update:exchangeRatesSourceKey',
+  ],
+
+  data() {
+    return {
+      toggleIconSrc: toggleIconSrc,
+    };
+  },
+
+  computed: {
+    amountValue: {
+      get() {
+        return this.amount;
+      },
+      set(newValue) {
+        this.$emit('update:amount', newValue);
+      },
+    },
+  },
+
+  methods: {
+    handleTogglerClick() {
+      const currentSourceCurrencyCharCode = this.sourceCurrencyCharCode;
+      $emit('update:sourceCurrencyCharCode', this.targetCurrencyCharCode);
+      $emit('update:sourceCurrencyCharCode', currentSourceCurrencyCharCode);
+    },
+  },
+
+  watch: {},
+};
+</script>
+
+<style scoped>
+.converter {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  /* start new stacking context to use absolute for loading-panel */
+  position: relative;
+}
+
+.converter__field-set {
+  border: none;
+  padding: 0;
+  margin: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 30px;
+}
+
+.converter__currency-toggler {
+  align-self: center;
+}
+
+.converter__warning-message {
+  /* reset browser styles */
+  padding: 0;
+  margin: 0;
+
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translate(-50%, -20%);
+  width: 100%;
+  text-align: center;
+  z-index: -1;
+
+  color: red;
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.converter__target-amount {
+  /* reset browser styles */
+  padding: 0;
+  margin: 0;
+
+  margin-top: 30px;
+
+  text-align: center;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 30px;
+}
+
+.converter__values {
+  border: none;
+  padding: 0;
+  margin: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 30px;
+}
+</style>
